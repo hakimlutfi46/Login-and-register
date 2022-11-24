ee2718fc79b7377861cf872c8b122dbec6769847
Lutfi Hakim <lutfihakim1606@gmail.com>
2022-11-24 13:46:55 +0700

    first commit

---
+++ src/miniproject/Config.java
@@ -0,0 +1,62 @@
+package miniproject;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ROG
+ */
+
+//import com.mysql.cj.jdbc.Driver;
+//import .*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+//import java.sql.Statement;
+import java.sql.SQLException;
+
+
+
+
+public class Config {
+
+    private static Connection mysqlconfig;
+    public static Connection configDB() throws SQLException{
+    
+        try {
+        String url ="jdbc:mysql://localhost:3307/data_mhs";
+        String user = "root";
+        String pass = "";
+        
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            mysqlconfig=DriverManager.getConnection(url, user, pass);
+        }catch (Exception e){
+        System.err.println("Koneksi Gagal "+ e.getMessage());
+        
+        }
+        return mysqlconfig;
+    }
+
+}  
+
+    
+    
+    
+//    public static Connection con;
+//    public static Statement stm;
+//    public static void main(String args[]){
+//        try {
+//            String url ="jdbc:mysql://localhost:3307/data_mhs";
+//            String user="root";
+//            String pass="";
+//            Class.forName("com.mysql.jdbc.Driver");
+//            con =DriverManager.getConnection(url,user,pass);
+//            stm = con.createStatement();
+//            System.out.println("koneksi berhasil;");
+//        } catch (Exception e) {
+//            System.err.println("koneksi gagal " +e.getMessage());
+//        }
+//    }
+//}
